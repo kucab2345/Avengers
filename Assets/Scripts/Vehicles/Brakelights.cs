6515d592791c7cdd09cdcc0e1c478988552bfd0f
kucab2345 <kucab2345@gmail.com>
2017-12-19 22:49:45 -0600

    Initial Push

---
+++ Assets/Scripts/Vehicles/Brakelights.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brakelights : MonoBehaviour {
+
+    private Light tailLight;
+    private float originalIntensity;
+
+	// Use this for initialization
+	void Start () {
+        tailLight = GetComponent<Light>();
+        originalIntensity = tailLight.intensity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            tailLight.intensity = originalIntensity;
+        }
+        else
+        {
+            tailLight.intensity = 0f;
+        }
+    }
+}
